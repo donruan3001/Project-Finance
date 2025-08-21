50e8c0f8313c71c1e3f280dd3957a47730321cf0
Ruan Valdez <106835563+donruan3001@users.noreply.github.com>
2025-08-21 19:41:46 -0300

    Review Project Structure and Start Frontend

--- finance-frontend/finance-frontend/src/services/dashboardService.ts
+++
@@ -1,9 +0,0 @@
-import api from './api';
-import { Dashboard } from '../types';
-
-export const dashboardService = {
-  async getDashboard(userId: number): Promise<Dashboard> {
-    const response = await api.get<Dashboard>(`/dashboard/user/${userId}`);
-    return response.data;
-  }
-};
